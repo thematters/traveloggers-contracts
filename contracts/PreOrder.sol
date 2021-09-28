9920b5fd51f3bf82cac051c11ac9b9237130829e
carolusian <pkufashuo400@gmail.com>
2021-09-29 01:15:08 +0800

    add PoC pre-order flow

--- contracts/PreOrder.sol
+++ contracts/PreOrder.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-abstract contract PreOrder {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract PreOrder is Ownable {
     // TBC: 
     // - whether to support unique addresses using Set-like data structure
     // - whether need to store pre-order amount (probably not, as pre-order is for the purpose of whitelist)
@@ -10,13 +12,78 @@ abstract contract PreOrder {
 
     // stage-1: basic functions to append to, and query an array of addresses
     // stage-2: think about the TBCs
-    mapping(address => bool) private _preOrderAddrs;
 
-    function preOrder(address _addr) public {
-        _preOrderAddrs[_addr] = true;
+    // TBC: add pre-order status field?
+    struct Participant {
+        address sender;
+        uint deposit;
+        uint8 round;
+    } 
+    
+    // the key `_preOrderIt` is incremented with each additional pre-order
+    // make sure `_preOrderIt` starts from 1 instead of 0
+    mapping(address => uint) private _preOrdered;
+    mapping(uint => Participant) private _preOrders;
+    uint private _nextPreOrder;
+    // TBC: add depositedTotal
+
+    // current round for pre-ordering, reserved, if only 1 round, then, this value can always be 1
+    // TO REMOVE, as it makes the logic complicated
+    uint8 private _round; 
+
+    // determine whether there is an ongoing pre-order 
+    bool public inPreOrder;
+
+    // error - pre-order is not available
+    error PreOrderNotStarted();
+    // error - pre-order is not available
+    error PreOrderAlreadyStarted();
+    // error - the owner shall not trigger an incorrect round
+    error IncorrectRound(uint8 previousRound);
+    error PreOrderExists(address addr);
+
+    // `round` argument is required make sure the owner knows what he is doing
+    function startPreOrder(uint8 round) public onlyOwner {
+        if (inPreOrder == true) revert PreOrderAlreadyStarted();
+        // only reopen existing round, or start next round        
+        if (round == 0) revert IncorrectRound(_round);
+        if (round != _round && round != _round + 1) revert IncorrectRound(_round);
+        
+        inPreOrder = true;
+        _round = round;
     }
 
-    function queryPreOrder(address _addr) public view virtual returns (bool) {
-        return _preOrderAddrs[_addr];        
+    // `round` argument is required to make sure the owner knows which round he is closing
+    function endPreOrder(uint8 round) public onlyOwner {
+        if (inPreOrder != true) revert PreOrderNotStarted();
+        if (round != _round) revert IncorrectRound(_round);
+        
+        inPreOrder = false;
+    } 
+
+    function currentRound() public view onlyOwner returns (uint8) {
+        if (inPreOrder != true) revert PreOrderNotStarted();
+        return _round;
     }
+
+    function preOrder() public payable {
+        if (inPreOrder != true) revert PreOrderNotStarted();
+        if (_preOrdered[msg.sender] > 0) revert PreOrderExists(msg.sender);
+        // TBC: do we need to check the same round for the same address?
+        // TBC: what is the validation against the amount
+
+        // lets start the index from 1, since default uint in mapping is 0 in _preOrdered
+        if (_nextPreOrder == 0) _nextPreOrder = 1; 
+        _preOrders[_nextPreOrder] = Participant({
+            sender: msg.sender, deposit: msg.value, round: _round
+        });
+        _preOrdered[msg.sender] = _nextPreOrder;
+        _nextPreOrder += 1;
+    }
+
+    function preOrderQuery(address addr) public view virtual returns (bool) {
+        return _preOrdered[addr] > 0;        
+    }  
+
+    // function preOrderParticipants () public view returns (uint)
 }
--- test/Matty.test.js
+++ test/Matty.test.js
@@ -1,4 +1,9 @@
+const chai = require("chai")
+const web3 = require("web3")
 const { expect } = require("chai")
+const { solidity } = require("ethereum-waffle")
+
+chai.use(solidity)
 
 // Load compiled artifacts
 const Matty = artifacts.require("Matty")
@@ -22,10 +27,90 @@ contract("Matty", (accounts) => {
     expect((await this.matty.tokenURI(1)).toString()).to.equal(uri)
   })
 
-  it("query a pre-order address", async function() {
-      // testing pre-order participant
-      await this.matty.preOrder(accounts[0]);
-      expect((await this.matty.queryPreOrder(accounts[0]))).to.equal(true)
-      expect((await this.matty.queryPreOrder(accounts[1]))).to.equal(false)
+  it("start pre-order with correct round", async function() {
+      await expect(this.matty.startPreOrder(0)).to.be.reverted
+      await expect(this.matty.startPreOrder(2)).to.be.reverted
+
+      // start pre-order round 1
+      let started = await this.matty.inPreOrder()
+      expect(started).to.equal(false)
+      await this.matty.startPreOrder(1)
+      started = await this.matty.inPreOrder()
+      expect(started).to.equal(true)
+
+      await expect(this.matty.startPreOrder(2)).to.be.reverted
+      await expect(this.matty.endPreOrder(2)).to.be.reverted
+      
+      // close pre-order round 1 & restart it
+      await this.matty.endPreOrder(1)
+      started = await this.matty.inPreOrder()
+      expect(started).to.equal(false)
+      await this.matty.startPreOrder(1)
+      started = await this.matty.inPreOrder()
+      expect(started).to.equal(true)
+
+      // close pre-order round 1 & start round 2
+      await this.matty.endPreOrder(1)
+      started = await this.matty.inPreOrder()
+      expect(started).to.equal(false)
+      await this.matty.startPreOrder(2)
+      started = await this.matty.inPreOrder()
+      expect(started).to.equal(true)
+      
+      const round = await this.matty.currentRound()
+      expect(round.toNumber()).to.equal(2)
+      await this.matty.endPreOrder(2)
+      await expect(this.matty.currentRound()).to.be.reverted
+  })
+
+  it("pre-ordering", async function() {
+      // pre-order not started
+      await expect(this.matty.preOrder({
+        from: accounts[0],
+        value: web3.utils.toWei("0.5", "ether")
+      })).to.be.reverted
+
+      // start pre-order round 1
+      await this.matty.startPreOrder(1)
+      // success pre-order
+      await this.matty.preOrder({
+        from: accounts[0],
+        value: web3.utils.toWei("0.5", "ether")
+      })
+      // no duplicated pre-ordering
+      await expect(this.matty.preOrder({
+        from: accounts[0],
+        value: web3.utils.toWei("1.5", "ether")
+      })).to.be.reverted
+
+      // pre-order closed
+      await this.matty.endPreOrder(1)
+      await expect(this.matty.preOrder({
+        from: accounts[1],
+        value: web3.utils.toWei("0.5", "ether")
+      })).to.be.reverted
+
+      // start pre-order round 2
+      await this.matty.startPreOrder(2)
+      await this.matty.preOrder({
+        from: accounts[1],
+        value: web3.utils.toWei("0.5", "ether")
+      })
+
+      // still, no duplicate order from acount 0
+      await expect(this.matty.preOrder({
+        from: accounts[1],
+        value: web3.utils.toWei("0.5", "ether")
+      })).to.be.reverted
+
+      // query pre-order status
+      let status = await this.matty.preOrderQuery(accounts[0])
+      expect(status).to.equal(true)
+      status = await this.matty.preOrderQuery(accounts[1])
+      expect(status).to.equal(true)
+      status = await this.matty.preOrderQuery(accounts[3])
+      expect(status).to.equal(false)
+
+      //TODO: batch add more pre-orders for more testing
   })
 })
