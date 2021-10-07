d75b992c095555291be7f63b2c711a5d8fab49b0
Guo Liu <liuguomailbox@gmail.com>
2021-10-07 16:02:49 -0700

    add script to generate creatures

---
+++ scripts/generateCreature.js
@@ -0,0 +1,139 @@
+/**
+ * start of settings
+ */
+
+// races are nouns
+const races = ["one-eye toad", "two-antenna elephant", "three-leg horse"];
+
+// ideologies are adjectives
+const ideologies = [
+  "left wing",
+  "right wing",
+  "anarchist",
+  "pacifist",
+  "nihilists",
+  "royalist",
+];
+
+// characters are adjectives
+const characters = [
+  "imaginative",
+  "wanderlusty",
+  "sentimental",
+  "practical",
+  "faithful",
+];
+
+// background colors in hex code
+const background_colors = ["f8edcd", "c2aa3e", "f8edcd", "008080"];
+
+// names
+const names = [
+  "Zoe",
+  "Jetee",
+  "Tartar",
+  "Horogu",
+  "Konggo",
+  "Teleluya",
+  "Zzuween",
+  "Kuuhuru",
+  "Teteerha",
+  "Dongmaguru",
+  "Honga",
+  "Tamara",
+  "Erwa",
+  "Junkun",
+];
+
+/**
+ * end of settings
+ */
+
+const Articles = require("articles");
+const fs = require("fs");
+const path = require("path");
+
+const totalSupply = names.length;
+
+/**
+ * Draw random element and remove it from array.
+ * @param {bool} replacement: sample with replacement or not, default to true
+ * @returns element been drawn
+ */
+Array.prototype.sample = function (replacement = true) {
+  // random index
+  const index = Math.floor(Math.random() * this.length);
+  const el = this[index];
+
+  // remove element
+  if (!replacement) {
+    this.splice(index, 1);
+  }
+
+  return el;
+};
+
+/**
+ * Capitalize first letter of a string
+ * @returns the capitalized string
+ */
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+// path to metadata
+const metatdataPath = path.join(__dirname, "..", "assets", "metadata");
+
+// main function
+const main = async () => {
+  if (totalSupply > races.length * ideologies.length * characters.length) {
+    throw Error(`Not enough attributes to generate ${totalSupply} creatures.`);
+  }
+
+  // exhaust all combination as candidates
+  let candidates = [];
+  races.map((race) =>
+    ideologies.map((ideology) =>
+      characters.map((character) =>
+        candidates.push({ race, ideology, character })
+      )
+    )
+  );
+
+  for (let id = 1; id <= totalSupply; id++) {
+    // random sample one combination
+    const { race, character, ideology } = candidates.sample(false);
+
+    // assemble metadata
+    const creature = {
+      name: names.sample(false),
+      description: `${Articles.articlize(
+        character
+      ).capitalize()} ${ideology} ${race} called ${names[id - 1]}.`,
+      background_color: background_colors.sample(),
+      image: `${id}.jpeg`,
+      attributes: [
+        {
+          trait_type: "Race",
+          value: race,
+        },
+        {
+          trait_type: "Ideology",
+          value: ideology,
+        },
+        {
+          trait_type: "Character",
+          value: character,
+        },
+      ],
+    };
+
+    // write to file
+    fs.writeFileSync(
+      path.join(metatdataPath, `${id}`),
+      JSON.stringify(creature, null, 2)
+    );
+  }
+};
+
+main();
